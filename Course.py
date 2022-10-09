dfa34d58552914b83623b1bd2b8401d4da6fc786
DhruvChauhanS <103396813+DhruvChauhanS@users.noreply.github.com>
2022-10-09 18:26:12 +1100

    ^

--- Course.py
+++ Course.py
@@ -1,5 +1,10 @@
 class Course:
 
-    def __init__(self):
-        
+    def __init__(self,course_code,title,credit_points,prerequisites):
+        self.c_code
+        self.c_title
+        self.c_points
+        self.c_pre
+        self.c_sem
+        self.c_list
         
