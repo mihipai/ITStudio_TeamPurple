e7ea0a2c9a9fcea489813b0c413abe852dea8701
KelvinDuongLy <s3953996@student.rmit.edu.au>
2022-10-18 20:23:52 +1100

    Testing student class and changes

--- Student.py
+++ Student.py
@@ -13,53 +13,71 @@ class Student:
         self.current_enrollment = current_enrollment
         self.study_plan = study_plan
 
-    def set_name(self, name):
+    def set_name(self, name = ''):
         self.name = name
     def get_name(self):
         return self.name
     def print_name(self):
-        print(self.name)
+        print(f'Name: {self.name}')
 
-    def set_student_id(self, student_id):
+    def set_student_id(self, student_id = ''):
         self.student_id = student_id
     def get_student_id(self):
         return self.student_id
     def print_student_id(self):
-        print(self.student_id)
+        print(f'Student ID: {self.student_id}')
 
-    def set_DOB(self, DOB):
+    def set_DOB(self, DOB = ''):
         self.DOB = DOB
     def get_DOB(self):
         return self.DOB
     def print_DOB(self):
-        print(self.DOB)
+        print(f'Date of birth: {self.DOB}')
     
-    def set_program_code(self, program_code):
+    def set_program_code(self, program_code = ''):
         self.program_code = program_code
     def get_program_code(self):
         return self.program_code
     def print_program_code(self):
-        print(self.program_code)
+        print(f'Currently enrolled program: {self.program_code}')
     
-    def set_academic_history(self, academic_history):
+    def set_academic_history(self, academic_history = []):
         self.academic_history = academic_history
     def get_academic_history(self):
         return self.academic_history
     def print_academic_history(self):
-        print(self.academic_history)
+        print(f'Your academic history: {self.academic_history}')
 
-    def set_current_enrollment(self, current_enrollment):
+    def set_current_enrollment(self, current_enrollment = []):
         self.current_enrollment = current_enrollment
     def get_current_enrollment(self):
         return self.current_enrollment
     def print_current_enrollment(self):
-        print(self.current_enrollment)
+        print(f'Your current enrollment: {self.current_enrollment}')
 
-    def set_study_plan(self, study_plan):
+    def set_study_plan(self, study_plan = []):
         self.study_plan = study_plan
     def get_study_plan(self):
         return self.study_plan
     def print_study_plan(self):
-        print(self.study_plan)
+        print(f'Your current study plan: {self.study_plan}')
 
-    
+    def print_all(self):
+        String = ''
+        String += 'Name: ' + self.name
+        String += '\nStudent ID: ' + self.student_id
+        String += '\nDate of birth: ' + self.DOB
+        String += '\nCurrently enrolled program: ' + self.program_code
+        String += '\nAcademic History: ' + self.academic_history
+        String += '\nCurrent enrollment: ' + self.current_enrollment
+        String += '\nStudy plan: ' + self.study_plan
+        print(String)
+
+#Testing stuff
+student1 = Student()
+name = 'Kelvin'
+Subject_list = ['English', 'Math', 'Chemistry', 'Biology']
+
+student1.set_name(name)
+student1.print_name()
+student1.set_academic_history()
