527317e7d167972b2887cd26d4c092690298d73f
MIHIKA PAI <s3918007@student.rmit.edu.au>
2022-10-20 14:07:25 +1100

    Welcome message + Login Option

--- main_menu.py
+++ main_menu.py
@@ -5,3 +5,40 @@ from Program import Program
 from Semester import Semester
 from Student import Student
 import math
+
+class Admin():
+
+    def __init__(self, adm_num):
+        self.admin_num = adm_num
+
+class InvLoginOption(Exception):
+    def __init__(self, mssg):
+        self.mssg = mssg
+
+print('===============================================')
+print('          WELCOME TO ENROLMENT ONLINE          ')
+print('===============================================')
+
+##adm1 = Admin()
+print('To Login as Student, type S')
+print('To Login as Admin, type A')
+logtype=input()
+if logtype == 'S':
+    ##Something to redirect student to Student class
+    print('Enter Student Number:')
+elif logtype == 'A':
+    ##Redirects to Admin class 
+    print('Enter Admin Number')
+    adm = input()
+    adm1 = Admin(adm)
+else:
+    raise InvLoginOption('Invalid Login Option. Restart to Login again')
+       
+
+
+
+
+
+
+
+
