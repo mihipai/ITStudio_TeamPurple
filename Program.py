10c36855d165ca38f963cdf7c78b89bf4d133a18
thil17 <s3888450@student.rmit.edu.au>
2022-10-20 14:06:46 +1100

    prograqm class add

--- Program.py
+++ Program.py
@@ -1,2 +1,9 @@
 #Thil is testing commit
 # This file will have the program class
+class Program:
+    def __init__(self,program_code,program_name,course_pre,program_elec,program_credits):
+        self.p_code = program_code
+        self.p_name = program_name
+        self.p_pre = course_pre
+        self.p_elec = program_elec
+        self.p_credits = program_credits
