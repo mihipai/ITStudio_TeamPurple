53bf7e8121cdc72dd4aff8f4c55a50fdee187265
Julia Phan <s3948825@student.rmit.edu.com>
2022-10-21 23:09:16 +1100

    Changed layout of objects within Semester class

--- Semester.py
+++ Semester.py
@@ -6,6 +6,8 @@ import csv
 class CourseOffering: #Calculation in adding/removing a student from a specific course offer
     def __init__(self,course_code,year,semester, max_students, student_list):
         load_data1 = Load_data()
+        self.courseinfo = Load_data.load_courses() 
+        #could add course information inside CourseOffering class string
         self.course_code = course_code
         self.year = year
         self.semester = semester
@@ -61,18 +63,18 @@ class CourseOffering: #Calculation in adding/removing a student from a specific 
 
     def __str__(self):
         formatted_str = ''
-        formatted_str += 'Course Code: ' + self.get_CourseCode() + '\n'
-        formatted_str += 'Year: ' + self.get_Year() + '\n'
-        formatted_str += 'Semester: ' + self.get_Semester() + '\n'
-        formatted_str += 'Max No Student: ' +  self.get_MaxStudents() + '\n'
-        formatted_str += 'List of Enrolled Students:\n'
+        formatted_str += 'Course Code: ' + self.get_CourseCode()
+        formatted_str += '\nYear: ' + self.get_Year()
+        formatted_str += '\nSemester Title: ' + self.get_Semester()
+        formatted_str += '\nMaximum Number of Students: ' +  self.get_MaxStudents()
+        formatted_str += '\nList of Enrolled Students:\n'
         for enrol in self.get_EnrolledStudents():
             formatted_str += enrol
         return formatted_str
 
 
 class Load_data:
-    def load_courses(filename):
+    def load_courses(filename): #Courses csv file
         with open(filename, 'r') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',')
             courselist = [] # list of course objects
@@ -83,17 +85,36 @@ class Load_data:
                     continue
                 course_code = row[0]
                 course_name = row[1]
-                credit_points = int(row[2])
-                prereq = row[3] #Unsure that it exists in database?
-                ava_sem = row[4]
+                credit_points = int(row[4])
+                prereq = row[3] #need to include course description too
+                ava_sem = row[5]
                 course_object = Course(course_name, course_code, credit_points, prereq, ava_sem)
                 courselist.append(course_object) 
             return courselist
 
-    def load_students(filename, course_code, year, semester):
+    def load_students(filename, course_codes, year, semester): #Students csv file
         with open(filename, 'r') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',')
             studentlist = [] # list of course objects
+            filterstudent = []
+            first_row = True
+            for row in csv_reader:
+                if first_row:
+                    first_row = False
+                    continue
+                name = row[1]
+                student_id = row[0]
+                dob = row[3]
+                program_code = row[4]
+                academic_history = row[7]
+                current_enrollment = row[5]
+                study_plan = row[6]
+                student_object = Student(name, student_id, dob, program_code, academic_history, current_enrollment, study_plan)
+                studentlist.append(student_object)
+            #read all students objects
+            #loop through student object list and find those with match course_code, year, semester
+            
+            formatted_str = ''
             first_row = True
             for row in csv_reader:
                 if first_row:
@@ -101,17 +122,23 @@ class Load_data:
                     continue
                 data_cell6 = row[5]
                 small_row = data_cell6.split(',')
+                year == small_row[0]
+                semester == small_row[1]
                 course_codes = list(small_row[2:])
-                if year == small_row[0] and semester == small_row[1]:
-                    if course_code in course_codes:
-                        year = small_row[0]
-                        semester = small_row[1]
+                formatted_str += year + ','
+                formatted_str += semester + ','
+                for course in course_codes:
+                    if not course == course_codes[-1]:
+                        formatted_str += course + ','
+                    else:
+                        formatted_str += course
+                for student in studentlist:
+                    if formatted_str == student.get_current_enrollment(): 
+                    #trying to call current enrollment inside student object from Student class
+                        filterstudent.append(student)
+            return filterstudent
 
-                        student_object = Student(year, semester, course_codes)
-                        studentlist.append(student_object)
-            return studentlist
-
-    def load_course_offerings(filename, filename2, course_code, year, semester):
+    def load_course_offerings(filename, filename2, course_code, year, semester): #Courses & Students csv files
         max_students_value = dict()
         #with to load here
         with open(filename2, 'r') as csvfile:
@@ -122,7 +149,7 @@ class Load_data:
                     first_row = False
                     continue
                 coursecode = row[0]
-                maxno = row[3]
+                maxno = row[6]
                 max_students_value[coursecode] = maxno
 
         with open(filename, 'r') as csvfile:
@@ -146,7 +173,6 @@ class Load_data:
                         course_offerings.append(course_offering)
             return course_offerings
 
-#Semester class is really complex, so I need help with it.
 # This file will have the Semester class
 class Semester:
     def __init__(self, semester_title, max_students,year):
@@ -162,35 +188,34 @@ class Semester:
     def get_SemesterTitle(self):
         return self.semester_title
     def print_SemesterTitle(self):
-        print(f'Current Semester: {self.semester_title}')
+        print('Current Semester:', self.semester_title)
 
     def set_CourseOfferings(self, course_offerings=[]):
         self.course_offerings = course_offerings
     def get_CourseOfferings(self):
         return self.course_offerings
     def print_CourseOfferings(self):
-        print(f'List of available Course Offerings: {self.course_offerings}')
+        print('List of available Course Offerings:', self.course_offerings)
 
     def set_MaxStudents(self, max_students=0):
         self.max_students = max_students
     def get_MaxStudents(self):
         return self.max_students
     def print_MaxStudents(self):
-        print(f'Maximum number of students: {self.max_students}')
+        print('Maximum number of students:', self.max_students)
 
     def set_EnrolledStudents(self, enrolled_students=[]):
         self.enrolled_students = enrolled_students
     def get_EnrolledStudents(self):
         return self.enrolled_students
     def print_EnrolledStudents(self):
-        print(f'List of currently enrolled students: {self.enrolled_students}')
+        print('List of currently enrolled students:', self.enrolled_students)
 
     def __str__(self):
-        string = f'{self.get_SemesterTitle()}\n'
+        string = self.get_SemesterTitle() + '\n'
         for course in self.course_offerings:
             string += course + '\n' 
         return string
-
 
 #Testing class
 '''
